d90f7c0d80136cfdbfd3e3946035ef26a97650cd
sandym <91996681+sandyml@users.noreply.github.com>
2023-02-18 13:54:52 -0500

    commit tree 5a6dfb6ee3723e8c051c76bed433d202261c7c9a

--- app/controllers/leases_controller.rb
+++ app/controllers/leases_controller.rb
@@ -1,15 +1,32 @@
 class LeasesController < ApplicationController
 
  def index
-  render json: Lease.all
+  render json: Tenant.all, status: :ok
  end
 
  def create
-  
+  lease = Lease.create(lease_params)
+  if lease.valid?
+   render json: lease, status: :ok
+  else
+   render json: {errors: lease.errors.full_messages }, status: :unprocessable_entity
+  end
  end
 
  def destroy
-  
+  lease = Lease.find_by(id: params[:id])
+   if lease
+    lease.destroy
+    render head :no_content
+   else
+    render json: { error: ["Lease not found"] }, status: :not_found
+   end
  end
- 
+
+ private
+
+ def lease_params
+  params.permit(:rent, :tenant_id, :apartment_id)
+ end
+
 end
